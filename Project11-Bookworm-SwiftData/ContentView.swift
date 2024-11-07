201b293ec8f6715b8171f8c5261bead1b8ec1233
Ibraheem <ibraheem.r@outlook.com>
2024-11-07 09:08:00 -0500

    Initial Commit

---
+++ Project11-Bookworm-SwiftData/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Project11-Bookworm-SwiftData
+//
+//  Created by Ibraheem k. Rawlinson on 11/7/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Project11-Bookworm-SwiftData/Project11_Bookworm_SwiftDataApp.swift
@@ -0,0 +1,17 @@
+//
+//  Project11_Bookworm_SwiftDataApp.swift
+//  Project11-Bookworm-SwiftData
+//
+//  Created by Ibraheem k. Rawlinson on 11/7/24.
+//
+
+import SwiftUI
+
+@main
+struct Project11_Bookworm_SwiftDataApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
